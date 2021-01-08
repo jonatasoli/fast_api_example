c49764d5cfa987c90246396418832195792adee4
Jônatas Oliveira <contato@jonatasoliveira.me>
2021-01-08 16:59:31 -0300

    initial commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from config import settings
+
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url="/auth",
+    redoc_url=None
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
